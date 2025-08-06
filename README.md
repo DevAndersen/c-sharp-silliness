a8981290468a9132b847468e8a4a680d3f6e83f8
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-08-06 10:09:26 +0200

    Added project ByValueSleep

--- README.md
+++ README.md
@@ -48,6 +48,22 @@ Doable? Yes.
 
 Useful? As expected, a resounding "no".
 
+### [ByValueSleep](./src/ByValueSleep/Program.cs)
+
+Value types are passed by value (shocking, I know), meaning that the time it takes to pass them to a method depends on the size of the structure.
+
+If we were to create a really big struct, could we use this to approximate a 1-second delay simply by passing our struct around?
+
+**Conclusion:** Yes, we can approximate a 1-second sleep simply by measuring the time it takes to pass a really bug `struct` around.
+
+With a 100,000 byte struct, we can narrow down the approximate number of times we need to pass it around before reaching a roughly 1-second delay.
+
+100,000 bytes is also well short of the usual 1 MB size of the execution stack, meaning we are completely safe from `StackOverflowException`.
+
+This all of course depends on the particular CPU being used, what it is currently doing besides this project, and so on. But, technically, this works.
+
+Note: This is one of the reasons why it is recommended that `struct` types should be small (16 bytes or less seems to be a common recommendation).
+
 ## Failed projects
 
 These are projects that did not work out (which is probably for the best).
---
+++ src/ByValueSleep/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+// The number of attempts to hit close enough to a 1-second delay.
+int attempt = 1;
+
+// The maximum number of attempts we'll do. We don't want to be here all day.
+int maxAttempts = 25;
+
+// The number of we'll pass the struct around for. Let's start with 100, it's a nice round number.
+int loops = 100;
+
+// The number of seconds it took to pass our struct {loop} number of times.
+double measuredTime = 0;
+
+// The error margin we're aiming for. Anything closer than this is assumed to be "close enough" to a 1-second delay.
+double errorMargin = 0.003;
+
+do
+{
+    measuredTime = StructSleeper.Sleep(loops);
+    loops = (int)(1 / (measuredTime / loops));
+    Console.WriteLine($"Attempt {attempt}: {loops} loops took {measuredTime} seconds");
+}
+while (double.Abs(measuredTime - 1) > errorMargin && ++attempt < maxAttempts);
+
+Console.WriteLine($"Settled on {loops} after {attempt} attempts");
+
+double finalMeasure = StructSleeper.Sleep(loops);
+Console.WriteLine($"Sleep took {finalMeasure} seconds, which is reasonably close to a second");
+
+/// <summary>
+/// Allows for putting the current thread to sleep by spending time passing large value objects around.
+/// </summary>
+public static class StructSleeper
+{
+    /// <summary>
+    /// Sleep for <paramref name="loops"/> loops.
+    /// </summary>
+    /// <param name="loops"></param>
+    /// <returns>The number of seconds it took to sleep.</returns>
+    public static double Sleep(int loops)
+    {
+        Sleeper sleeper = new Sleeper();
+        Stopwatch sw = Stopwatch.StartNew();
+
+        for (int i = 0; i < loops; i++)
+        {
+            SleepInner(sleeper);
+        }
+
+        sw.Stop();
+        return sw.Elapsed.TotalSeconds;
+
+        static void SleepInner(Sleeper sleeper)
+        {
+        }
+    }
+
+    /// <summary>
+    /// A very big struct.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Size = 100_000)]
+    public ref struct Sleeper
+    {
+    }
+}
